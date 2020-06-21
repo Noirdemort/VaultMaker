e41d5a0acdbc8473a20f61c8ec3ed2c710274453
noirdemort <darkStillwater@protonmail.com>
2020-06-21 15:58:13 +0530

    Initial Commit

---
+++ VaultMaker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VaultMaker
+//
+//  Created by Noirdemort on 21/06/20.
+//  Copyright © 2020 Noirdemort. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
